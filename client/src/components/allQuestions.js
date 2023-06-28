ad12225782c335db60723ee7b2f8b522a2000ab7
ryanrosenberg <ryan.rosenberg@outlook.com>
2023-06-28 11:53:09 -0700

    add bonuses

--- client/src/components/allQuestions.js
+++ client/src/components/allQuestions.js
@@ -8,7 +8,17 @@ export default function AllQuestions() {
   const questionData = require("./sample-all-questions.json");
   const numPackets = [1, 2, 3, 4, 5];
 
-  var cats = _.groupBy(questionData.questions, "category");
+  var cats = _.groupBy(_.filter(
+    questionData.questions,
+    (question) => question.type !== "bonus"
+  ), "category");
+  var bcats = _.groupBy(
+    _.filter(
+      questionData.questions,
+      (question) => question.type === "bonus"
+    ),
+    "category"
+  );
   const all_subcats = {
     Literature: [
       "American Literature",
@@ -55,77 +65,180 @@ export default function AllQuestions() {
                   {all_subcats[cat].map(function (subcat_name) {
                     console.log(_.groupBy(cats[cat], "subcategory"));
                     return (
-                      <tr className={tables.subcatRow}>
-                        <td className={tables.headerColumn}>{subcat_name}</td>
-                        {subcat_name in _.groupBy(cats[cat], "subcategory")
-                          ? numPackets
-                              .map((x) => x - 1)
-                              .map((q_num) => {
-                                let q = _.groupBy(cats[cat], "subcategory")[
-                                  subcat_name
-                                ][q_num];
-                                if (
-                                  q_num + 1 >
-                                  _.groupBy(cats[cat], "subcategory")[
+                      <>
+                        <tr className={tables.subcatRow}>
+                          <td className={tables.headerColumn}>
+                            {subcat_name + " Tossups"}
+                          </td>
+                          {subcat_name in _.groupBy(cats[cat], "subcategory")
+                            ? numPackets
+                                .map((x) => x - 1)
+                                .map((q_num) => {
+                                  let q = _.groupBy(cats[cat], "subcategory")[
                                     subcat_name
-                                  ].length
-                                ) {
-                                  return (
-                                    <td
-                                      className={classnames(
-                                        tables.questionCell,
-                                        tables.unclaimed
-                                      )}
-                                    >
-                                      {""}
-                                      <br />
-                                      <button className={tables.questionStatus}>
-                                        unclaimed
-                                        <FaChevronDown
-                                          className={tables.arrowIcon}
-                                        />
-                                      </button>
-                                    </td>
-                                  );
-                                } else {
-                                  return (
-                                    <td
-                                      className={classnames(
-                                        tables.questionCell,
-                                        tables[q.status]
-                                      )}
-                                    >
-                                      {q.answer ? q.answer : q.slot}
-                                      <br />
-                                      <div className={tables.questionStatusRow}>
-                                      <button className={tables.questionStatus}>
-                                        {q.status}
-                                        <FaChevronDown
-                                          className={tables.arrowIcon}
-                                        />
-                                      </button>
-                                      <span>{q.author ? `<${q.author}>`:''}{q.editor ? ` |${q.editor}|`:''}</span>
-                                      </div>
-                                      
-                                    </td>
-                                  );
-                                }
-                              })
-                          : numPackets
-                              .map((x) => x - 1)
-                              .map((q_num) => (
-                                <td className={classnames(tables.questionCell, tables.unclaimed)}>
-                                  {""}
-                                  <br />
-                                  <button className={tables.questionStatus}>
-                                    unclaimed
-                                    <FaChevronDown
-                                      className={tables.arrowIcon}
-                                    />
-                                  </button>
-                                </td>
-                              ))}
-                      </tr>
+                                  ][q_num];
+                                  if (
+                                    q_num + 1 >
+                                    _.groupBy(cats[cat], "subcategory")[
+                                      subcat_name
+                                    ].length
+                                  ) {
+                                    return (
+                                      <td
+                                        className={classnames(
+                                          tables.questionCell,
+                                          tables.unclaimed
+                                        )}
+                                      >
+                                        {""}
+                                        <br />
+                                        <button
+                                          className={tables.questionStatus}
+                                        >
+                                          unclaimed
+                                          <FaChevronDown
+                                            className={tables.arrowIcon}
+                                          />
+                                        </button>
+                                      </td>
+                                    );
+                                  } else {
+                                    return (
+                                      <td
+                                        className={classnames(
+                                          tables.questionCell,
+                                          tables[q.status]
+                                        )}
+                                      >
+                                        {q.answer ? q.answer : q.slot}
+                                        <br />
+                                        <div
+                                          className={tables.questionStatusRow}
+                                        >
+                                          <button
+                                            className={tables.questionStatus}
+                                          >
+                                            {q.status}
+                                            <FaChevronDown
+                                              className={tables.arrowIcon}
+                                            />
+                                          </button>
+                                          <span>
+                                            {q.author ? `<${q.author}>` : ""}
+                                            {q.editor ? ` |${q.editor}|` : ""}
+                                          </span>
+                                        </div>
+                                      </td>
+                                    );
+                                  }
+                                })
+                            : numPackets
+                                .map((x) => x - 1)
+                                .map((q_num) => (
+                                  <td
+                                    className={classnames(
+                                      tables.questionCell,
+                                      tables.unclaimed
+                                    )}
+                                  >
+                                    {""}
+                                    <br />
+                                    <button className={tables.questionStatus}>
+                                      unclaimed
+                                      <FaChevronDown
+                                        className={tables.arrowIcon}
+                                      />
+                                    </button>
+                                  </td>
+                                ))}
+                        </tr>
+                        <tr className={tables.subcatRow}>
+                          <td className={tables.headerColumn}>
+                            {subcat_name + " Bonuses"}
+                          </td>
+                          {subcat_name in _.groupBy(bcats[cat], "subcategory")
+                            ? numPackets
+                                .map((x) => x - 1)
+                                .map((q_num) => {
+                                  let q = _.groupBy(bcats[cat], "subcategory")[
+                                    subcat_name
+                                  ][q_num];
+                                  if (
+                                    q_num + 1 >
+                                    _.groupBy(bcats[cat], "subcategory")[
+                                      subcat_name
+                                    ].length
+                                  ) {
+                                    return (
+                                      <td
+                                        className={classnames(
+                                          tables.questionCell,
+                                          tables.unclaimed
+                                        )}
+                                      >
+                                        {""}
+                                        <br />
+                                        <button
+                                          className={tables.questionStatus}
+                                        >
+                                          unclaimed
+                                          <FaChevronDown
+                                            className={tables.arrowIcon}
+                                          />
+                                        </button>
+                                      </td>
+                                    );
+                                  } else {
+                                    return (
+                                      <td
+                                        className={classnames(
+                                          tables.questionCell,
+                                          tables[q.status]
+                                        )}
+                                      >
+                                        {q.answer ? q.answer : q.slot}
+                                        <br />
+                                        <div
+                                          className={tables.questionStatusRow}
+                                        >
+                                          <button
+                                            className={tables.questionStatus}
+                                          >
+                                            {q.status}
+                                            <FaChevronDown
+                                              className={tables.arrowIcon}
+                                            />
+                                          </button>
+                                          <span>
+                                            {q.author ? `<${q.author}>` : ""}
+                                            {q.editor ? ` |${q.editor}|` : ""}
+                                          </span>
+                                        </div>
+                                      </td>
+                                    );
+                                  }
+                                })
+                            : numPackets
+                                .map((x) => x - 1)
+                                .map((q_num) => (
+                                  <td
+                                    className={classnames(
+                                      tables.questionCell,
+                                      tables.unclaimed
+                                    )}
+                                  >
+                                    {""}
+                                    <br />
+                                    <button className={tables.questionStatus}>
+                                      unclaimed
+                                      <FaChevronDown
+                                        className={tables.arrowIcon}
+                                      />
+                                    </button>
+                                  </td>
+                                ))}
+                        </tr>
+                      </>
                     );
                   })}
                 </>
