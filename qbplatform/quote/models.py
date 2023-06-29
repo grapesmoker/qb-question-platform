6a8b39f73aa5438debe36bd3241c1ba888df6fea
Jonathen Settle <settlejonathen@gmail.com>
2023-06-29 14:35:44 -0400

    Initial Distribution model

--- qbplatform/quote/models.py
+++ qbplatform/quote/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import User
+
+from tree_queries.models import TreeNode
 # Create your models here.
 
 
@@ -36,6 +38,30 @@ class Packet(models.Model):
         return "Packet " + self.name + " in tournament: " + str(self.tournament)
 
 
+class Distribution(models.Model):
+
+    number_packets = models.PositiveSmallIntegerField(null=True)
+    number_tossups_per_packet = models.PositiveSmallIntegerField(null=True, default=20)
+    number_bonuses_per_packet = models.PositiveSmallIntegerField(null=True, default=20)
+
+    # a tournament/question set should only have one distribution
+    tournament = models.OneToOneField(Tournament, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.tournament) + " distribution"
+
+
+class Category(TreeNode):
+
+    name = models.CharField(max_length=100)
+    number_tossups = models.PositiveSmallIntegerField(null=True)
+    number_bonuses = models.PositiveSmallIntegerField(null=True)
+    distribution = models.ForeignKey(Distribution, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name + " for " + self.distribution.tournament.name
+
+
 class Tossup(models.Model):
 
     number = models.IntegerField(null=True, unique=True)
@@ -49,6 +75,13 @@ class Tossup(models.Model):
     )
 
     packet = models.ForeignKey(Packet, on_delete=models.CASCADE)
+
+    #TU difficulty rating
+    difficulty = models.PositiveSmallIntegerField(null=True)
+
+    #Don't want to cascade delete because distribution might be adjusted after questions are written?
+    #We could lock distribution on tournament creation, but that seems too restrictive
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return "Tossup " + str(self.number) + " of packet: " + str(self.packet)
@@ -68,6 +101,10 @@ class Bonus(models.Model):
             MaxValueValidator(5),
         ]
     )
+
+    #Don't want to cascade delete because distribution might be adjusted after questions are written?
+    #We could lock distribution on tournament creation, but that seems too restrictive
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return "Bonus " + str(self.number) + " of packet: " + str(self.packet)
