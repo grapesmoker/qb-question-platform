b15a37695c9f74bc43592c1d6cd6a3855697c151
Jerry Vinokurov <grapesmoker@gmail.com>
2023-07-21 21:39:19 -0400

    wip

--- qbplatform/qbplatform/urls.py
+++ qbplatform/qbplatform/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from quote.views import tossup_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tossups/', tossup_list)
 ]
