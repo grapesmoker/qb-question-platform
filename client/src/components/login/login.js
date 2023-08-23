230b51fdb472d2f3530207719a3036bbca056659
ryanrosenberg <ryan.rosenberg@outlook.com>
2023-08-23 12:48:26 -0700

    wip

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,6 +1,34 @@
 import styles from "./login.module.css";
+import "js-cookie";
 
 export default function Login() {
+  function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+      const cookies = document.cookie.split(";");
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) === name + "=") {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  }
+  function doLogin() {
+    const csrftoken = getCookie("csrftoken");
+    var headers = new Headers();
+    headers.append("X-CSRFToken", csrftoken);
+    fetch("/api/upload", {
+      method: "POST",
+      body: ,
+      headers: headers,
+      credentials: "include",
+    });
+  }
+
   return (
     <div className={styles.loginSectionDiv}>
       <h2>Login</h2>
@@ -26,7 +54,9 @@ export default function Login() {
         <a href="https://hsquizbowl.org/forums/">Forgot password?</a>
       </div>
       <div className={styles.loginRegisterDiv}>
-        <button className={styles.loginButton}>Log Into QUOTE</button>
+        <button className={styles.loginButton} onClick={doLogin}>
+          Log Into QUOTE
+        </button>
         <a href="https://hsquizbowl.org/forums/">No account? Register here</a>
       </div>
     </div>
--- qbplatform/qbplatform/settings.py
+++ qbplatform/qbplatform/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
@@ -39,9 +45,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
